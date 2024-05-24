ad14b30a729f549deb8601bf8b41f03e9766b0c6
gabriellecronofy <gabrielle@cronofy.com>
2024-05-24 14:31:39 +0100

    Shortcut model updated to correct structure.

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -1,6 +1,38 @@
+require "entity_store"
+
 class Shortcut
   include Entity
 
-  def create(by, at, shortcut, shortcut_name)
+  def self.create(by, at1, shortcut, shortcut_name)
+    shortcut = Shortcut.new
+    shortcut.record_event ShortcutCreated.new(
+      at2: at1,
+      by: by,
+      shortcut_name: shortcut_name,
+    )
+  end
+
+  def created_at=(value)
+    @at = value
+  end
+
+  def created_by=(value)
+    @by = value
+  end
+
+  def shortcut_name=(value)
+    @shortcut_name = value
+  end
+
+  class ShortcutCreated
+    include EntityEvent
+
+    attr_accessor :at2, :by, :shortcut_name
+
+    def apply(entity)
+      entity.created_at = @at2
+      entity.created_by = @by
+      entity.shortcut_name = @shortcut_name
+    end
   end
 end
