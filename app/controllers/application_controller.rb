e7fd9ed2035d64546e582b925623b17f29d63362
gabriellecronofy <gabrielle@cronofy.com>
2024-06-21 12:14:08 +0100

    Updated controller and view to show tabel of shortcuts. Hard coded the shortcut due to entity_store setup error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Cronofy::Stores
+      def entity_store
+        @_entity_store ||= EntityStore::Store.new
+      end
 end
--- app/controllers/shortcuts_controller.rb
+++ app/controllers/shortcuts_controller.rb
@@ -1,5 +1,20 @@
+# require "entity_store
+
 class ShortcutsController < ApplicationController
   def show
-    @people = ["Adam", "Gabby"]
+      id = "66744938a5db6f75840ca57b"
+      @shortcut = entity_store.get(id)
+  rescue => e 
+    rails.logger.error "It doesn't work, the reason is #{e.message} and #{e.class}"
   end
 end
+
+# class ShortcutsController < ApplicationController
+#   def show
+#     @shortcuts = []
+    # entity_store.all_ids({}) do |id|
+      # entity = entity_store.get(id)
+      # @shortcuts << entity if entity.is_a? Shortcut
+  #   end
+  # end
+# end
